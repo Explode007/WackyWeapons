5a913c2c65f05ede796310b6f35a4bb1bfc08e8a
Explode007 <samaldonador@gmail.com>
2022-09-21 10:14:26 -0400

    First Commit

---
+++ src/main/java/com/frilledshrimpo/wackyweapons/client/util/ClientUtils.java
@@ -0,0 +1,9 @@
+package com.frilledshrimpo.wackyweapons.client.util;
+
+import net.minecraftforge.api.distmarker.*;
+
+@OnlyIn(Dist.CLIENT)
+public class ClientUtils {
+    
+}
+
---
+++ src/main/java/com/frilledshrimpo/wackyweapons/core/init/BlockInit.java
@@ -0,0 +1,5 @@
+package com.frilledshrimpo.wackyweapons.core.init;
+
+public class BlockInit {
+    
+}
